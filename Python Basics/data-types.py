6585aad3e721e43e9d885228ea3b7454d67a7186
Benja <benjaminokumu12@gmail.com>
2022-05-01 18:36:23 +0300

    data-types

---
+++ Python Basics/data-types.py
@@ -0,0 +1,29 @@
+# Data types in python include
+
+# Integers - they are numbers eg 10, 20
+print(29)
+b = 7
+print(b, 'is of type', type(b))
+
+#Complex no.
+a = 1 + 2j
+print(a, "is complex number", isinstance(1+2j, complex))
+
+#Floating-point Numbers - no. with decimal points
+print(4.5)
+print(3.142)
+
+#Strings - sequence of character data
+print("I am Benjamin")
+print('Benjamin is a software engineer')
+
+# List - is ordered sequence of items. They are mutable
+a = [1, 1.2, 'Benjamin']
+print(a)
+
+#Tuple - ordered sequence of items that are immutable
+t = (5, 'progran', 1+3j)
+
+#Set - unordered collection of unique items
+a = {5, 3, 1, 5}
+print(type(a))
