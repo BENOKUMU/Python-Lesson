4b9247d0df8a5b2455e8251fe411c53fdbd35b92
Benja <benjaminokumu12@gmail.com>
2022-05-16 22:32:33 +0300

    js

--- Challenges/fizzBuzz.js
+++ Challenges/fizzBuzz.js
@@ -4,3 +4,14 @@ for (let i = 1; i <= 100; i++){
     if (i % 5 == 0) out += 'Buzz';
     console.log(out || i);
 }
+
+function FizzBuzz(aTarget) {
+    for (var i = 1; i <= aTarget; i++) {
+        var result = "";
+        if (i%3 === 0) result += "Fizz";        
+        if (i%5 === 0) result += "Buzz";
+        if (result.length ===0) result = i;
+
+        console.log(result);
+    }
+}
