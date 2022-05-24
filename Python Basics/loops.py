26366251b901d2ebef2eca711117244d68ef98b9
Benja <benjaminokumu12@gmail.com>
2022-05-25 01:00:02 +0300

    py

---
+++ Python Basics/loops.py
@@ -0,0 +1,22 @@
+
+count = 0
+while (count < 9):
+    print("The count is:", count)
+    count = count + 1
+print("Good bye!")
+
+
+var = 1
+while var == 1:
+    num = int(input("Enter a number: "))
+    print("You entered: ")
+print("Good bye!")
+
+
+for letter in 'Benjamin':
+    print("current letter:", letter)
+print()
+fruits = ['banana', 'apple', 'mango']
+for fruits in fruits:
+    pritn('current fruit:, fruit')
+print('Good bye!')
