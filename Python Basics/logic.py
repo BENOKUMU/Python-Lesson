c867d4ac6786c3cb48327325b12a03446b26ff0a
Benja <benjaminokumu12@gmail.com>
2022-05-25 00:17:36 +0300

    py

---
+++ Python Basics/logic.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+list = [1,2,3,4,5]
+
+if (a in list):
+    print("a present")
+else:
+    print(" a absent ")
+    
+if (b not in list):
+    print("a absent")
+else:
+    print("a present")
+    
+    
+c = b/a
+if (c in list):
+    print("c present")
+else:
+    print("c absent")
