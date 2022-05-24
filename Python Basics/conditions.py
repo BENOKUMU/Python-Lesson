8e92c8d2a8367661f5d18ae2f93ef30c44e70d4f
Benja <benjaminokumu12@gmail.com>
2022-05-25 00:39:20 +0300

    py

--- Python Basics/conditions.py
+++ Python Basics/conditions.py
@@ -1,15 +1,15 @@
-# var1 = 100
-# if var1:
-#     print("1 - Got a true expression value")
-#     print(var1)
+var1 = 100
+if var1:
+    print("1 - Got a true expression value")
+    print(var1)
     
     
-# var2 = 0
-# if var2:
-#     print("2 - Got a true expression value")
-#     print(var2)
+var2 = 0
+if var2:
+    print("2 - Got a true expression value")
+    print(var2)
     
-# print("Good bye!")
+print("Good bye!")
 
 
 
@@ -22,3 +22,37 @@ else:
     print("Discount", discount)
     
 print ("Net payable:", amount - discount)
+
+
+
+
+amount = int(input("Enter amount: "))
+if amount < 1000:
+    discount = amount * 0.05
+    print("Discount", discount)
+elif amount < 5000:
+    discount = amount * 0.10
+    print("Discount", discount)
+else:
+    discount = amount * 0.10
+    print("Discount", discount)
+    
+print ("Net payable:", amount - discount)
+
+
+
+if mu%2==0:
+    if num%3==0:
+        print("Divisible by 3 and 2")
+    else:
+        print("divisible by 2 not divisible by 3")
+else:
+    if num%3==0:
+        print("divisible by 3 not divisible by 2")
+    else:
+        print("divisible by 2 and not 3")
+
+
+var = 100
+if (var == 100): print("value of expression is 100")
+print("Good Bye!")
