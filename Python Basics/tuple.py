57da86119f4a71efaa93c302295eefee03039ee1
Benja <benjaminokumu12@gmail.com>
2022-05-25 13:39:32 +0300

    py

---
+++ Python Basics/tuple.py
@@ -0,0 +1,16 @@
+
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = ("a", "b", "c", "d")
+
+print(tup1[0])
+print(tup2[3])
+
+tup = ('physics', 'chemistry', 1997, 2000);
+print (tup)
+del tup
+print ("After deleting tup : ")
+print (tup)
+
+len(tup1)
+max(tup1)
