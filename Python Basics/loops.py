85fd462c8c1d047088a73e8270290f9572c35371
Benja <benjaminokumu12@gmail.com>
2022-05-25 07:53:30 +0300

    py

--- Python Basics/loops.py
+++ Python Basics/loops.py
@@ -18,5 +18,30 @@ for letter in 'Benjamin':
 print()
 fruits = ['banana', 'apple', 'mango']
 for fruits in fruits:
-    pritn('current fruit:, fruit')
+    print('current fruit:, fruit')
 print('Good bye!')
+
+numbers = [11,33,55,39,55,74,23,21,41,13]
+for num in numbers:
+    if num%2 == 0:
+        print('The list contain an even number')
+        break
+else:
+    print('The list doesnt contain even number')
+
+for i in range(1,11):
+    for j in range(1,11):
+        k=i*j
+        print(k, end= ' ')
+    print()
+
+no = int(input('any number: '))
+numbers = [11,33,55,39,55,74,23,21,41,13]
+for num in numbers:
+    if num == no:
+        print('number found in list')
+        break
+    else:
+        print('number not found in list')
+        break
+
